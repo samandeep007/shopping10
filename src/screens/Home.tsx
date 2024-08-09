1659086aa5b5fa360216ec71b3bbabc999d61a01
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-09 17:01:30 -0600

    refactor: Improve styling and layout of Detail screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const Home = () => {
+//React Navigation
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { RootStackParamList } from '../App'
+
+//Components
+import ProductItem from '../components/ProductItem'
+import Separator from '../components/Separator'
+
+//Constant
+import { PRODUCTS_LIST } from '../data/constants'
+
+type HomeProps = NativeStackScreenProps<RootStackParamList, "Home">
+
+const Home = ({ navigation }: HomeProps) => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={PRODUCTS_LIST}
+        keyExtractor={item => item.id}
+        ItemSeparatorComponent={Separator}
+        renderItem={({ item }) => (
+          <Pressable onPress={() => navigation.navigate('Details', {
+            product: item
+          })}>
+            <ProductItem product={item} />
+          </Pressable>
+        )}
+      />
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    padding: 12,
+    backgroundColor: '#FFFFFF',
+  },
+})
 export default Home
-
-const styles = StyleSheet.create({})
