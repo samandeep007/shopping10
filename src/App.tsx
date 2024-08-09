b711147f9e6108e7d1defdbfeb76ebdb86918c99
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-09 15:53:21 -0600

    Refactor App.tsx to remove unused imports and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
+type HomeProps = NativeStackNavigationProp<{
+  
+}>
 
 export default function App() {
   return (
