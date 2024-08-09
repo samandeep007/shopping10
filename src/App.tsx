6342a84c72f52114e03d157bc5e50f2f0c6aeedc
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-09 16:03:36 -0600

    refactor: Remove unused imports and styles in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
+//Navigation
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+//Screens
+import Home from './screens/Home';
+import Detail from './screens/Detail';
+
+export type RootStackParamList = {
+  Home: undefined;
+  Details: {product: Product}
+}
 const Stack = createNativeStackNavigator();
 
 
@@ -10,8 +21,8 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-
+    <>
+    </>
   )
 }
 
-const styles = StyleSheet.create({})
