58cfb0814d41b96cfed88c86d9795c6a771fad27
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-09 16:08:27 -0600

    refactor: Update navigation in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,8 +22,12 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <>
-    </>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Home' component={Home} options={{title: "Trending Products"}}/>
+        <Stack.Screen name='Details' component={Detail} options={{title: "Product Details"}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
