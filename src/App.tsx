7cb94a4e805429cfbdccd2459e00dc93d9351a9c
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-09 15:56:56 -0600

    chore: Update npm dependencies for @react-navigation/native, @react-native-safe-area-context, and @react-native-screens

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,13 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
-type HomeProps = NativeStackNavigationProp<{
-  
-}>
+
+
+
 
 export default function App() {
   return (
-   <Stack.Navigator>
-    <Stack.Screen name='home' component={} />
-    <Stack.Screen/>
-   </Stack.Navigator>
+
   )
 }
 
